f4824cf63df3f693c5e668b3b340c59720926084
KKIMYENN <yenny031003@gmail.com>
2024-05-25 04:00:44 +0900

    4시

--- pardyz/src/API/Axios.js
+++ pardyz/src/API/Axios.js
@@ -5,7 +5,31 @@ export const getAllInfoAPI = async () => {
   try {
     const response = await axios.get(`${apiEndpoint}/post`);
     console.log(response.data);
+    return response.data;
   } catch (error) {
     console.error(error);
   }
+};
+
+export const postInfoAPI = async (data) => {
+    try {
+        const response = await axios.post (`${apiEndpoint}/post`,{
+            params:{data}
+        });
+        console.log (response);
+        console.log (response.data);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+export const getOneInfoAPI = async(id) => {
+    try {
+        const response = await axios.get (`${apiEndpoint}/post/${id}`);
+        console.log(response.data);
+        return response.data;
+;    } catch (error) {
+        console.error(error);
+    }
 };
--- pardyz/src/Sidebar.js
+++ pardyz/src/Sidebar.js
@@ -3,24 +3,50 @@ import { Outlet } from "react-router-dom";
 
 const Sidebar = () => {
   return (
-    <>
+    <BigDiv>
       <Div>
         <div>카테고리</div>
-        <div>
+        <Menus>
           <div>여행</div>
           <div>요리</div>
           <div>머리</div>
           <div>패션</div>
           <div>맛집</div>
-        </div>
+        </Menus>
       </Div>
-    </>
+      <Outlet />
+    </BigDiv>
   );
 };
 
+const BigDiv = styled.div`
+  display: flex;
+  white-space: nowrap;
+`;
 const Div = styled.div`
+  gap: 23.6px;
   width: 153px;
   height: auto;
-  background: beige;
+  display: flex;
+  flex-direction: column;
+
+  font-size: 34px;
+  color: #000;
+  font-family: "Apple SD Gothic Neo";
+  font-size: 34px;
+  font-style: normal;
+  font-weight: 600;
 `;
+
+const Menus = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: #222;
+  font-family: "Hiragino Kaku Gothic Pro";
+  font-size: 22px;
+  font-style: normal;
+  font-weight: 300;
+  gap: 19.89px;
+`;
+
 export default Sidebar;

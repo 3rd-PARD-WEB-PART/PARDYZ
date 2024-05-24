4c470d78a6947d539e351024498042821e282bb2
KKIMYENN <145086605+KKIMYENN@users.noreply.github.com>
2024-05-25 04:01:13 +0900

    Kye (#4)
    * 작업
    
    * test~
    
    * div 수정
    
    * 4시

--- pardyz/src/Sidebar.js
+++ pardyz/src/Sidebar.js
@@ -1,21 +1,52 @@
 import styled from "styled-components";
-import {Outlet, NavLink} from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 const Sidebar = () => {
-  return(
-  <>
-    <Div>
-      <div>카테고리</div>
-      <NavLink />
-    </Div>
-    
-  </>
+  return (
+    <BigDiv>
+      <Div>
+        <div>카테고리</div>
+        <Menus>
+          <div>여행</div>
+          <div>요리</div>
+          <div>머리</div>
+          <div>패션</div>
+          <div>맛집</div>
+        </Menus>
+      </Div>
+      <Outlet />
+    </BigDiv>
   );
 };
 
+const BigDiv = styled.div`
+  display: flex;
+  white-space: nowrap;
+`;
 const Div = styled.div`
+  gap: 23.6px;
   width: 153px;
   height: auto;
-  background: beige;
+  display: flex;
+  flex-direction: column;
+
+  font-size: 34px;
+  color: #000;
+  font-family: "Apple SD Gothic Neo";
+  font-size: 34px;
+  font-style: normal;
+  font-weight: 600;
 `;
+
+const Menus = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: #222;
+  font-family: "Hiragino Kaku Gothic Pro";
+  font-size: 22px;
+  font-style: normal;
+  font-weight: 300;
+  gap: 19.89px;
+`;
+
 export default Sidebar;

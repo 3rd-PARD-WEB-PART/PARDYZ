fbfe424c8bae1abb49bae24ef5c97929d1285623
Dongun614 <72856107+Dongun614@users.noreply.github.com>
2024-05-25 05:58:38 +0900

    menuBar 구현중

--- pardyz/src/Components/MenuBar.js
+++ pardyz/src/Components/MenuBar.js
@@ -1,9 +1,25 @@
 function MenuBar() {
   return(
-    <>
-    네 이겁니다.
-    </>
+    <Bar>
+      <BarButton></BarButton>
+
+    </Bar>
   )
 }
+
+const Bar = styled.div`
+width: 866px;
+height:37px;
+top: 214px;
+left: 323px;
+padding: 10px;
+gap: 10px;
+border-radius: 6px;
+opacity: 0px;
+display: flex;
+`
+
+const BarButton = styled.div`
+`
 
 export default MenuBar;

f9e3fa3593b50719bc1fad0aa8e478900453c901
Dongun614 <72856107+Dongun614@users.noreply.github.com>
2024-05-25 08:19:30 +0900

    Jdw (#10)
    * setting
    
    * start
    
    * Components -> Pages 폴더명 변경
    
    * 라우팅
    
    * routing
    
    * 글로벌 스타일
    
    * 작업
    
    * MainPage 레이아웃 구성
    
    * Kye (#1)
    
    * 작업
    
    * test~
    
    * Hym (#2)
    
    * first commit of yumin!
    
    * feat: DetailPage
    
    * 컴포넌트 분리
    
    * Jdw (#3)
    
    * start
    
    * 작업
    
    * MainPage 레이아웃 구성
    
    * 컴포넌트 분리
    
    * Kye (#4)
    
    * 작업
    
    * test~
    
    * div 수정
    
    * 4시
    
    * 레이아웃 구현
    
    * Hym (#5)
    
    * first commit of yumin!
    
    * feat: DetailPage
    
    * fix: layout
    
    * feat: new css
    
    * Jdw (#6)
    
    * start
    
    * 작업
    
    * MainPage 레이아웃 구성
    
    * 컴포넌트 분리
    
    * 레이아웃 구현
    
    * Jdw (#7)
    
    * start
    
    * 작업
    
    * MainPage 레이아웃 구성
    
    * 컴포넌트 분리
    
    * 레이아웃 구현
    
    * Writing 구현
    
    * menuBar 구현중
    
    * menubar
    
    * 메인페이지 완성
    
    ---------
    
    Co-authored-by: KKIMYENN <yenny031003@gmail.com>
    Co-authored-by: KKIMYENN <145086605+KKIMYENN@users.noreply.github.com>
    Co-authored-by: Yoomin Hwang <134265429+yoomin-hwang@users.noreply.github.com>

--- pardyz/src/Pages/MainPage.js
+++ pardyz/src/Pages/MainPage.js
@@ -4,6 +4,10 @@ import GlobalStyles from "../GlobalStyles";
 import BoardItem from "../Components/BoardItem";
 import { getAllInfoAPI } from "../API/Axios";
 import { useState } from "react";
+import { Link } from "react-router-dom";
+import MenuBar from "../Components/MenuBar";
+import CreateButton from "../Components/CreateButton";
+
 
 const MainPage = () => {
   const tempList = [
@@ -33,12 +37,22 @@ const MainPage = () => {
   
 
 return (
+  <Whole>
   <Container>
+      <Svg>
+        <Link to="/detail">  
+          <CreateButton/> 
+        </Link>
+      </Svg>
+      {/* <Tiara/>
       <Title>실패 전시회</Title>
       <SubTitle>카테고리/홈</SubTitle>
+      <MenuBar/>
       <SecondHeader>
         <BoardTitle>금주의 실패작</BoardTitle>
-        <Create>작품 올리러 가기</Create> 
+        <Link to="/detail">
+          <Create>작품 올리러 가기</Create> 
+        </Link>
       </SecondHeader>
       <Board>
       
@@ -46,18 +60,47 @@ return (
             return <BoardItem key={item.id} member={item}/>;
           })}
       
-      </Board>
+      </Board> */}
 
   </Container>
+  </Whole>
 );
 };
 
-const Container = styled.div`
+const Svg = styled.div`
+&:hover{
+  cursor: pointer;
+}
+margin-top:200px;
+margin-left: 750px;
+display: flex;
+`
+
+const Whole = styled.div`
+background-image: url("/MainPage.png");
+background-size: cover;
 width: 1512px;
+height: 1370px;
+// top: 900px;
+gap: 0px;
+opacity: 0px;
+`
+
+const Tiara = styled.div`
+width: 120px;
+height: 40px;
+display: flex;
+background-image: url("/Vector.png");
+background-size: cover;
+margin-bottom: 30px;
+`
+
+const Container = styled.div`
+
 flex-direction: column;
 display: flex;
 align-items: center;
-margin-top:99px;
+margin-top:90px;
 `
 
 const Title = styled.div`
@@ -72,7 +115,7 @@ font-size: 50px;
 font-weight: 600;
 line-height: 18px;
 text-align: center;
-color: #929292;
+color: #E8E8E8;
 margin-bottom: 20px;
 `
 
@@ -88,38 +131,49 @@ font-size: 16px;
 font-weight: 500;
 line-height: 18px;
 text-align: center;
-color: #929292;
+margin-bottom: 10px;
+color: #E8E8E8;
 `
 
 const Create = styled.button`
-width: 200px;
-height: 54px;
-top: 240px;
-left: 1120px;
-padding: 21px 18.75px 15px 19.25px;
+width: 150px;
+height: 15px;
+top: 347px;
+left: 1040px;
+padding: 18px 19px 19px 19px;
 gap: 0px;
+border-radius: 6px;
 opacity: 0px;
-background-color: #E8E8E8;
-color: #929292;
+background: linear-gradient(180deg, #FFBD3D 0%, #FFBD3D 78.94%, #9E7018 143.52%);
+box-shadow: 0px 4px 4px 0px #00000040;
+&:hover{
+  cursor: pointer;
+}
 display: flex;
+font-family: 210 Cheoeumcheoreom;
+font-size: 22px;
+font-weight: 400;
+line-height: 18px;
+text-align: center;
 justify-content: center;
-align-items: center;
 `
 const BoardTitle = styled.div`
 width: 267px;
 height: 17px;
-top: 271px;
-left: 348px;
+top: 365px;
+left: 283px;
 gap: 0px;
+border: 0.4px 0px 0px 0px;
 opacity: 0px;
-font-family: Apple SD Gothic Neo;
+font-family: 210 Cheoeumcheoreom;
 font-size: 32px;
-font-weight: 600;
+font-weight: 400;
 line-height: 18px;
+text-align: center;
 display:flex;
 justify-content: left;
-color: #929292;
-margin-top: 50px;
+color: #E8E8E8;
+margin-top: 30px;
 `
 
 const SecondHeader = styled.div`

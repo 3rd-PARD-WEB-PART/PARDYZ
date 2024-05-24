91180ab1f1ee6305031d105c228a7f13147c4c16
Dongun614 <72856107+Dongun614@users.noreply.github.com>
2024-05-25 03:03:17 +0900

    컴포넌트 분리

--- pardyz/src/Pages/MainPage.js
+++ pardyz/src/Pages/MainPage.js
@@ -1,18 +1,19 @@
 import React from "react";
 import styled from "styled-components";
 import GlobalStyles from "../GlobalStyles";
+import BoardItem from "../Components/BoardItem";
 
 const MainPage = () => {
-  const tempList = [
-    {
-      id : 1,
-      name : "kim"
-    },
-    {
-      id : 1,
-      name : "park"
-    }
-  ];
+  // const tempList = [
+  //   {
+  //     id : 1,
+  //     name : "kim"
+  //   },
+  //   {
+  //     id : 1,
+  //     name : "park"
+  //   }
+  // ];
 return (
   <Container>
       <Title>실패 전시회</Title>
@@ -22,12 +23,7 @@ return (
         <Create>작품 올리러 가기</Create> 
       </SecondHeader>
       <Board>
-        <BoardItem>
-          <ItemImage/>
-          <Desc>
-            
-          </Desc>
-        </BoardItem>
+        <BoardItem/>
       </Board>
   </Container>
 );
@@ -98,7 +94,7 @@ font-size: 32px;
 font-weight: 600;
 line-height: 18px;
 display:flex;
-justify-content: center;
+justify-content: left;
 color: #929292;
 margin-top: 50px;
 `
@@ -114,29 +110,6 @@ const Board = styled.div`
 width: 976px;
 height: 830.79px;
 display : flex;
-`
-
-const BoardItem = styled.div`
-width: 306px;
-height: 258.93px;
-gap: 0px;
-opacity: 0px;
-background-color : yellow;
-display : block;
-`
-
-const ItemImage = styled.div`
-width: 306px;
-height: 234px;
-gap: 0px;
-background: #E1E1E1;
-display : flex;
-`
-
-const Desc = styled.div`
-display: flex;
-height: 23.93px;
-width: 50px;
 `
 
 export default MainPage;

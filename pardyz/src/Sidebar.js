1c1306c379ebbff1f0a797de05f767ee952fefb6
KKIMYENN <yenny031003@gmail.com>
2024-05-25 01:01:37 +0900

    작업

---
+++ pardyz/src/Sidebar.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Sidebar = () => {
+  return <Div>def</Div>;
+};
+
+const Div = styled.div`
+  width: 153px;
+  height: auto;
+  background: beige;
+`;
+export default Sidebar;

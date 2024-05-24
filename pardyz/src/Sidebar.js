3e47022181b9f84ecab64ddda17c656d335d0e27
KKIMYENN <yenny031003@gmail.com>
2024-05-25 03:11:28 +0900

    div 수정

--- pardyz/src/Sidebar.js
+++ pardyz/src/Sidebar.js
@@ -1,15 +1,20 @@
 import styled from "styled-components";
-import {Outlet, NavLink} from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 const Sidebar = () => {
-  return(
-  <>
-    <Div>
-      <div>카테고리</div>
-      <NavLink />
-    </Div>
-    
-  </>
+  return (
+    <>
+      <Div>
+        <div>카테고리</div>
+        <div>
+          <div>여행</div>
+          <div>요리</div>
+          <div>머리</div>
+          <div>패션</div>
+          <div>맛집</div>
+        </div>
+      </Div>
+    </>
   );
 };
 

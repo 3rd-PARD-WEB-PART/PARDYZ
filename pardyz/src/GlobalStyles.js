641ddc29b3cfd9bd529b3f3d1686d1ea93c2c114
KKIMYENN <yenny031003@gmail.com>
2024-05-24 23:22:36 +0900

    setting

---
+++ pardyz/src/GlobalStyles.js
@@ -0,0 +1,47 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+
+html {
+    font-size: 16px;
+    all: unset;
+}
+
+body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+input {
+    all: unset;
+    caret-color: #15521D;
+}
+
+div {
+    all: unset; 
+}
+
+select {
+    all: unset;
+}
+button {
+    all: unset;
+}
+
+h1 {
+    all: unset;
+}
+
+form {
+    all: unset;
+}
+/* :link {
+    all: unset;
+} */
+
+:any-link {
+    all: unset;
+}
+`;
+export default GlobalStyles;

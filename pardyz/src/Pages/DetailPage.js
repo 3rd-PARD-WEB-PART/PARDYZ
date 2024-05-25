b3d5707f7f65c6fb1b46d47cf8d221ddb8075fdc
KKIMYENN <145086605+KKIMYENN@users.noreply.github.com>
2024-05-25 09:38:25 +0900

    리코일 추가 (#14)

--- pardyz/src/Pages/DetailPage.js
+++ pardyz/src/Pages/DetailPage.js
@@ -11,194 +11,145 @@ const DetailPage = () => {
 
   useEffect(() => {
     const getInfo = async (id) => {
-      try{
+      try {
         const response = await getOneInfoAPI(id);
         setInfo(response);
         console.log(info);
-      } catch(err) {
+      } catch (err) {
         console.error(err);
       }
-      
     };
     getInfo(id);
   }, []);
 
   return (
     <Wrapper>
-      {/* <Outlet /> */}
-      <Header>
-        <img src="/top.png" width="10%" />
-        <img src="/logo.png" width="30%" />
-      </Header>
-      {/* <Subtitle>카테고리/요리</Subtitle> */}
       <Container>
         <Exhibit>
+          {/* 이미지 & 이름 */}
           <Div>
-            <File><img src="/file.png" width="50%" /></File>
             <Name>
-              <Label>{info && info.title}/{info && info.painter}</Label>
+              <Author>dd{info && info.title} </Author>
+              <PaintTitle>dd{info && info.painter}</PaintTitle>
             </Name>
           </Div>
-          <Desc>
-            <Label>✓ 내가 한 실패는?</Label>
-            {info && info.lacked}
-          </Desc>
-          <Line />
-          <Desc>
-            <Label>✓ 하지만 실패를 통해 이런 것들을 배웠어요!</Label>
-            {info && info.explanation}
-          </Desc>
-          <Line />
-          <Desc>
-            <Label>✓ 그래서 이런 점을 깨닫게 되었어요</Label>
-            {info && info.learned}
-          </Desc>
-          <Line />
-          <Desc>
-            <Label>✓ 실패를 반복하지 않기 위해, 이렇게 하겠습니다!</Label>
-            {info && info.longedfor}
-            <Comment>
-              <Label>작품에 대한 감상평을 남겨주세요!</Label>
-              <Nickname>
-                <>별명:</>
-                <Input type="textarea" placeholder="Add a comment...."></Input>
-              </Nickname>
-              <Input type="textarea" placeholder="Add a comment...."></Input>
-              <Button>작성완료</Button>
-            </Comment>
-          </Desc>
+
+          <Down>
+            <Desc>
+              <Label>✓ 내가 한 실패는?</Label>
+              <div>{info && info.lacked}</div>
+            </Desc>
+            <Line />
+            <Desc>
+              <Label>✓ 하지만 실패를 통해 이런 것들을 배웠어요!</Label>
+              <div>{info && info.explanation}</div>
+            </Desc>
+            <Line />
+            <Desc>
+              <Label>✓ 그래서 이런 점을 깨닫게 되었어요</Label>
+              <div>{info && info.learned}</div>
+            </Desc>
+            <Line />
+            <Desc>
+              <Label>✓ 실패를 반복하지 않기 위해, 이렇게 하겠습니다!</Label>
+              <div>{info && info.longedfor}</div>
+            </Desc>
+            <Line />
+          </Down>
         </Exhibit>
       </Container>
     </Wrapper>
   );
 };
 
-export const File = styled.div`
-display: flex;
-justify-content: center;
-width: auto;
-border-image-source : url("/border.png");
-`
-
 export const Header = styled.div`
-display: flex;
-flex-direction: column;
-align-items: center;
-justify-content: center;
-padding: 0px;
-margin-top: 10px;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 0px;
+  margin-top: 10px;
+`;
 
 export const Line = styled.hr`
-height: 1px;
-width: 449px;
-background-color: #E8E8E8;
-border: none;
-`
+  height: 1px;
+  width: 600px;
+  background-color: #e8e8e8;
+  border: none;
+  margin-bottom: 20px;
+`;
 
 const Wrapper = styled.div`
-width: 1512px;
-display: flex;
-flex-direction: column;
-align-items: center;
-justify-content: center;
-z-value: -1;
-background-image : url("/bg.png");
-background-repeat : no-repeat;
-background-size : cover;
-`
+  width: 1512px;
+  height: 2000px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  /* z-value: -1; */
+  background-image: url("/Detail.png");
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
 
 const Desc = styled.div`
-display: flex;
-flex-direction: column;
-width: 449px;
-margin-top: 90px;
-`
+  display: flex;
+  flex-direction: column;
+  width: 449px;
+  gap: 42px;
+`;
 
 export const Div = styled.div`
-display: flex;
-flex-direction: column;
-width: 469px;
-margin-top: 17px;
-`
+  display: flex;
+  flex-direction: column;
+  width: 469px;
+  margin-top: 450px;
+`;
+
+const Down = styled.div`
+  margin-top: 65px;
+`;
+
 export const Name = styled.div`
-display: flex;
-flex-direction: row;
-justify-content: flex-end;
-`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Author = styled.div`
+  color: #1b1b1b;
+  font-family: "Apple SD Gothic Neo";
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 500;
+`;
+
+const PaintTitle = styled.div`
+  color: #1b1b1b;
+  font-family: "Apple SD Gothic Neo";
+  font-size: 22px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 18px; /* 81.818% */
+`;
 
 const Exhibit = styled.div`
-display: flex;
-flex-direction: column;
-align-items: center;
-justify-content: center;
-width: 608px;
-`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 608px;
+`;
 
 const Container = styled.div`
-background-color: #FFFFFF;
-display: flex;
-flex-direction: column;
-align-items: center;
-justify-content: center;
-width: 1104px;
-margin-top: 30px;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 530px;
+`;
 
-const Title = styled.p`
-font-family: 'cheoeumcheoreom-210', sans-serif;
-font-style: normal;
-font-weight: 300;
-font-size: 42px;
-font-weight: 600;
-line-height: 18px;
-text-align: center;
-color: #929292;
-`
-
-const Subtitle = styled.p`
-font-size: 14px;
-font-weight: 500;
-line-height: 18px;
-text-align: center;
-color: #929292;
-margin: 0px;
-`
-
-export const Label = styled.p`
-font-size: 22px;
-font-weight: 500;
-line-height: 18px;
-margin-top: 17px;
-`
-
-const Comment = styled.div`
-display: flex;
-flex-direction: column;
-justify-content: flex-start;
-width: 469px;
-margin-top: 124px;
-`
-
-const Nickname = styled.div`
-display: flex;
-`
-
-const Input = styled.input`
-background-color: #F7F7F7;
-width: 100%;
-height: 36px;
-padding: 9px;
-`
-
-const Button = styled.button`
-background-color: #FFBD3D;
-color: #222222;
-
-text-align: center;
-width: 449px;
-height: 51px;
-font-weight: 500;
-`
+export const Label = styled.div`
+  font-size: 22px;
+  font-weight: 500;
+`;
 
 export default DetailPage;
